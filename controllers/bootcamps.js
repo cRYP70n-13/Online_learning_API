9afb0c8a693dd49c16f3e1bf4b7edf374cbb629a
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-07-25 16:31:00 +0100

    adding the controllers folder and the bootcamps controller to the project and filtring the routes also start implementing the logic for our API

---
+++ controllers/bootcamps.js
@@ -0,0 +1,44 @@
+/**
+ * @desc    Get all the bootcamps
+ * @route   GET /api/v1/bootcamps
+ * @access  Public
+ */
+exports.getBootcamps = (req, res, next) => {
+    res.status(200).json({success: true, message: "Show all the bootcamps"});
+}
+
+/**
+ * @desc    Get single bootcamp
+ * @route   GET /api/v1/bootcamp/:id
+ * @access  Public
+ */
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: `show the bootcamp ${req.params.id}`});
+}
+
+/**
+ * @desc    Create new bootcamp
+ * @route   POST /api/v1/bootcamp/:id
+ * @access  Private
+ */
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: `Create a new bootcamp`});
+}
+
+/**
+ * @desc    update a bootcamp
+ * @route   PUT /api/v1/bootcamp/:id
+ * @access  Private
+ */
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: `Update the bootcamp ${req.params.id}`});
+}
+
+/**
+ * @desc    Delete a bootcamp
+ * @route   DELETE /api/v1/bootcamp/:id
+ * @access  Private
+ */
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, message: `Deleting the bootcamp ${req.params.id}`});
+}
--- routes/bootcamps.routes.js
+++ routes/bootcamps.routes.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({success: true, message: "Show all the bootcamps"});
-});
+// Requiring the controllers folder
+const {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp
+} = require('../controllers/bootcamps');
 
-router.get('/:id', (req, res, next) => {
-    res.status(200).json({success: true, message: `show the bootcamp ${req.params.id}`});
-})
-router.post('/', (req, res, next) => {
-    res.status(200).json({success: true, message: `Create a new bootcamp`});
-});
+router
+    .route('/')
+    .get(getBootcamps)
+    .post(createBootcamp);
 
-router.put('/:id', (req, res, next) => {
-    res.status(200).json({success: true, message: `Update the bootcamp ${req.params.id}`});
-});
-
-router.delete('/:id', (req, res, next) => {
-    res.status(200).json({success: true, message: `Deleting the bootcamp ${req.params.id}`});
-});
+router
+    .route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
 
 module.exports = router;
