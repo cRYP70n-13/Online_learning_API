9b20744e400391d9e781cadc15ba32f04031945e
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-07-27 01:39:50 +0100

    adding some additional shit and some comments to the bootcamps controller file using the mongoose model

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,5 +1,6 @@
 /**
  * @desc    Logs Req to the console
+ * this is a middleware that we gonna use to perfrme our server
  */
 const logger = (req, res, next) => {
     console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
