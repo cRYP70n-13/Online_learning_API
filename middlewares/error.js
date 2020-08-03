6d35522e2c8fc4d2f458f7b83044c558d3d6cd04
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-03 20:38:13 +0100

    adding mongoose validation error without any third party error handler which is beter anyway

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,12 +4,23 @@ const errorHandler = (err, req, res, next) => {
 	let error = { ...err };
 
 	error.message = err.message;
-	console.log(err.stack.red);
 
 	// Mongoose bad object ID
 	if (err.name === 'CastError') {
 		const message = `Ressource not found with the id off ${err.value}`;
 		error = new errorResponse(message, 404);
+	}
+
+	// Mongoose dupliacte key
+	if (err.code === 11000) {
+		const message = `Duplicate field value entered`
+		error = new errorResponse(message, 400);
+	}
+
+	// Mongoose validation Error
+	if (err.name === 'VaidationError') {
+		const message = Object.values(err.errors).map(value => val.message);
+		error = new errorResponse(message, 400);
 	}
 
 	res.status(error.statusCode || 500).json({
