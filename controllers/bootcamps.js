9d2f8b1d3983aac758f00919b3f4436405a45b81
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-08 16:29:44 +0100

    Adding the pagination and the limit also the skip functionnalities

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,7 +13,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	const reqQuery = {...req.query};
 
 	// Fields to exclude
-	const removeFields = ['select', 'sort'];
+	const removeFields = ['select', 'sort', 'limit', 'page'];
 
 	// Loops over the remove fields and delete them from reqQuery
 	removeFields.forEach(param => delete reqQuery[param]);
@@ -41,6 +41,13 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	} else {
 		query = query.sort('-createdAt');
 	}
+
+	// Pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit) || 10;
+	const skip = (page - 1) * limit;
+
+	query = query.skip(skip).limit(limit);
 
 	// Executing the query
 	const bootcamps = await query;
