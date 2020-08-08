2821d2005a47926550d05ae7c350583cc5354b53
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-08 17:12:38 +0100

    adding the prev and next pages in pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -44,15 +44,37 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
 	// Pagination
 	const page = parseInt(req.query.page, 10) || 1;
-	const limit = parseInt(req.query.limit) || 10;
-	const skip = (page - 1) * limit;
+	const limit = parseInt(req.query.limit, 10) || 1;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await Bootcamp.countDocuments();
+	console.log(total);
 
-	query = query.skip(skip).limit(limit);
+	query = query.skip(startIndex).limit(limit);
 
 	// Executing the query
 	const bootcamps = await query;
+
+	// Pagination result
+	const pagination = {}
+
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit
+		}
+	}
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit
+		}
+	}
+
 	res.status(200).json({ 
 		succes: true,
+		count: bootcamps.length,
+		pagination,
 		data: bootcamps
 	});
 });
