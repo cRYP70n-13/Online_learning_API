d8d9099f38d01e7509d40d04cd577eae0454253a
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-11-20 06:54:00 +0100

    Adding the login and setting up our token matching the passwords and hashing the original one

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middlewares/async');
 
 /**
  * @desc Register User
- * @route GET /api/v1/auth/register
+ * @route POST /api/v1/auth/register
  * @access Public
  */
 exports.register = asyncHandler(async (req, res, next) => {
@@ -17,6 +17,44 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     });
+
+    // Create a token
+    const token = user.getSignedJwtToken();
+
+    res
+        .status(200)
+        .json({
+            success: true,
+            token
+        });
+})
+
+/**
+ * @desc Login User
+ * @route POST /api/v1/auth/register
+ * @access Public
+ */
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email and Password
+    if (!email || !password) {
+        return next(new ErrorResponse(`Please provid an email and a pass`, 400));
+    }
+
+    // Check for the user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorResponse(`No user created with this email`), 401);
+    }
+    
+    // Check the if the two password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse(`Invalid credentials`, 401));
+    }
 
     // Create a token
     const token = user.getSignedJwtToken();
--- models/User.js
+++ models/User.js
@@ -49,4 +49,9 @@ UserSchema.methods.getSignedJwtToken = function() {
     });
 }
 
+// Match passwords
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
 module.exports = mongoose.model('User', UserSchema);
