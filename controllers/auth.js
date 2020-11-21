61c1afab45aeb27c61082577e618acd9045a597b
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-11-21 06:42:13 +0100

    Adding the send email and not knowing where the fuck is the ERROR

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 const asyncHandler = require('../middlewares/async');
+const sendEmail = require('../utils/sendEmail');
 
 /**
  * @desc Register User
@@ -82,6 +83,31 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     const resetToken = user.getResetPasswordToken();
 
     await user.save({ validateBeforeSave: false });
+
+    // Create the reset URL to reset our token
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    // The message to send to the user
+    const message = `you are reciving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset Token',
+            message
+        });
+
+        res.status(200).json({ success: true, data: `Email sent successfully`});
+    } catch (error) {
+        console.log(error);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+        return next(
+            new ErrorResponse(`Email could not be sent !!`, 500)
+        )
+    }
 
     res.status(200).json({
         success: true,
