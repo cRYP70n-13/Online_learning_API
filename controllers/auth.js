9244ed817daf078d55f58a2f56e03ac4f2483b50
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-11-23 10:09:07 +0100

    Adding some modif on the auth module

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,6 +56,23 @@ exports.login = asyncHandler(async (req, res, next) => {
 })
 
 /**
+ * @desc Log user out and clear cookie
+ * @route GET /api//v1/auth/logout
+ * @access Private
+ */
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie('token', 'none', {
+        expires: new Date.now() + 10 * 1000,
+        httpOnly: true
+    })
+
+	res.status(200).json({
+		success: true,
+		data: user
+	})
+})
+
+/**
  * @desc GET current logged in User
  * @route POST /api/v1/auth/me
  * @access Private
